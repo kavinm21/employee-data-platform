5d533aefd951b5d755520e7a5c761f136c1d3bc6
Kavin M <89208380+kavinm21@users.noreply.github.com>
2023-02-04 09:23:10 +0530

    added skeleton code
    basic framework for flask app

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('login.html')
+
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+   if request.method == 'GET':
+      return redirect(url_for('home'))
+
+   if request.method == 'POST':
+      name = request.form['username']
+      age = request.form['password']
+   return render_template('index.html')
+
+@app.route('/logout')
+def logout():
+   return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
