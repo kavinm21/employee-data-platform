ca3a32ce388882ee3f87dcbe854f9d2f9d56a0d5
Kavin M <89208380+kavinm21@users.noreply.github.com>
2023-02-05 12:06:32 +0530

    added sql ops for read

--- sql-actions.py
+++ sql-actions.py
@@ -1,5 +1,17 @@
 import mysql.connector
 from mysql.connector import Error
+selectall_query = '''
+SELECT
+    e.id, e.first_name, e.last_name, e.date_of_birth, e.gender,
+    d.department_id, d.department_name, d.employee_role, d.employee_salary,
+    a.house_no, a.street_name, a.city, a.state, a.country, a.pincode
+FROM
+    employee e
+INNER JOIN department d ON
+    e.id = d.employee_id
+INNER JOIN address a ON
+    e.id = a.employee_id
+'''
 
 class DB_Ops:
     def __init__(self, host_name, user_name, pword):
@@ -9,16 +21,25 @@ class DB_Ops:
             print("Connected to MySQL")
         except Error as err:
             print(f"Error: '{err}'")
-    def read_emp(self, emp_id):
-        cursor = self.connection.cursor()
-        query = "SELECT * FROM employee where id = " + str(emp_id)
+    def fetch_one(self, emp_id):
+        cursor = self.connection.cursor()  
+        query = selectall_query + " where e.id = " + str(emp_id)
         cursor.execute(query)
-        print(type(cursor))
+        result = [dict((cursor.description[i][0], value) \
+               for i, value in enumerate(row)) for row in cursor.fetchall()]
         cursor.close()
+    def fetch_all(self):
+        cursor = self.connection.cursor()
+        cursor.execute(selectall_query)
+        result = [dict((cursor.description[i][0], value) \
+               for i, value in enumerate(row)) for row in cursor.fetchall()]
+        cursor.close()
+        return result
     def __del__(self):
         self.connection.close()
         print('Closing MySQL Connection')
 
 if __name__ == '__main__':
     sql = DB_Ops('127.0.0.1', 'flask', 'flask')
-    sql.read_emp(1)
+    data = sql.fetch_all()
+    print(data)
