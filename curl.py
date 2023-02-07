c2bc515e278905e367febdfd584ca6a0f8f0cc3f
Kavin M <89208380+kavinm21@users.noreply.github.com>
2023-02-07 19:55:10 +0530

    updated to get response

--- curl.py
+++ curl.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:5000/api/addEmp'
+url = 'https://employee-data-platform.vercel.app/api/fetchone'
 json = {"id": 105}
 x = requests.post(url, json=json)
-print(x)
+print(x.json())
