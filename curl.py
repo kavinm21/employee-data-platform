cea2187fa51bd0a622e8092a651b00df627a391c
shreya_ganeshe <78029387+shreya251170@users.noreply.github.com>
2023-02-07 21:41:29 +0530

    Update curl.py

--- curl.py
+++ curl.py
@@ -1,6 +1,33 @@
 import requests
 
-url = 'https://employee-data-platform.vercel.app/api/fetchone'
-json = {"id": 105}
+url = 'http://127.0.0.1:5001/api/addEmp'
+json = {
+    "city": "Chennai",
+    "country": "India",
+    "date_of_birth": "2003-02-20 00:00:00",
+    "department_id": 100010,
+    "department_name": "Finance",
+    "employee_role": "Finance Manager",
+    "employee_salary": 15000.0,
+    "first_name": "Shankar",
+    "gender": "Male",
+    "house_no": 100,
+    "id": 111,
+    "last_name": "M",
+    "pincode": 641001,
+    "state": "TN",
+    "street_name": "Srinagar"
+}
 x = requests.post(url, json=json)
-print(x.json())
+print(x)
+
+'''
+url = 'http://127.0.0.1:5001/api/updateDetails'
+json = {
+    "city" : "Hyderabad",
+    "id" : 105
+}
+
+x = requests.post(url, json=json)
+print(x)
+'''
