3f8aa9499321ea3f91d7f3b848ae95005b2b88bb
Kavin M <89208380+kavinm21@users.noreply.github.com>
2023-02-07 11:18:54 +0530

    updating to aws support

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def fetch_one():
       data = DB_Ops().fetch_one(id_key['id'])
       return jsonify(data)
    else:
-      return 'Content-Type not supported! Send JSON Content'
+      return 'Content-Type not supported! Send JSON Content\n'
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5001, debug=True)
