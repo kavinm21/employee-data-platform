c7de68ed8381be32b344add48a3504723d4e621c
Kavin M <89208380+kavinm21@users.noreply.github.com>
2023-02-09 18:59:01 +0530

    made main func accessible for local server use

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, session, url_for, j
 from sql_actions import DB_Ops
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
 def home():
@@ -10,40 +11,46 @@ def home():
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-   if request.method == 'GET':
-      return redirect(url_for('home'))
+    if request.method == 'GET':
+        return redirect(url_for('home'))
 
-   if request.method == 'POST':
-      name = request.form['username']
-      age = request.form['password']
-   return render_template('index.html')
+    if request.method == 'POST':
+        name = request.form['username']
+        age = request.form['password']
+    return render_template('index.html')
+
 
 @app.route('/logout')
 def logout():
-   return redirect(url_for('home'))
+    return redirect(url_for('home'))
+
 
 @app.route('/display')
 def display():
-   return render_template('display.html')
+    return render_template('display.html')
+
 
 @app.route('/update')
 def update():
-   return render_template('update.html')
+    return render_template('update.html')
+
 
 @app.route('/api/fetchall', methods=['GET', 'POST'])
 def fetch_all():
-   data = DB_Ops().fetch_all()
-   return jsonify(data)
+    data = DB_Ops().fetch_all()
+    return jsonify(data)
+
 
 @app.route('/api/fetchone', methods=['GET', 'POST'])
 def fetch_one():
-   content_type = request.headers.get('Content-Type')
-   if content_type == 'application/json':
-      id_key = request.json
-      data = DB_Ops().fetch_one(id_key['id'])
-      return jsonify(data)
-   else:
-      return 'Content-Type not supported! Send JSON Content'
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        id_key = request.json
+        data = DB_Ops().fetch_one(id_key['id'])
+        return jsonify(data)
+    else:
+        return 'Content-Type not supported! Send JSON Content'
+
 
 @app.route('/api/addEmp', methods=['POST', 'GET'])
 def add_employee():
@@ -57,28 +64,26 @@ def add_employee():
         result_addr = DB_Ops().insert_one_addr(emp_data)
         print(result_addr)
         if result_emp == 'Successfully inserted!!' and result_dept == 'Successfully inserted!!' and result_addr == 'Successfully inserted!!':
-         return "Successfully inserted!!"
+            return "Successfully inserted!!"
         else:
-         return "Oops!! Failed :/"
+            return "Oops!! Failed :/"
     else:
-      return 'Content-Type not supported! Send JSON Content'
+        return 'Content-Type not supported! Send JSON Content'
 
 
 @app.route('/api/delEmp', methods=['POST', 'GET'])
 def delete_details():
-      content_type = request.headers.get('Content-Type')
-      if content_type == 'application/json':
-         emp_data = request.json
-         data = DB_Ops().delete_one(emp_data)
-         if data=="Deleted Successfully!!":
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        emp_data = request.json
+        data = DB_Ops().delete_one(emp_data)
+        if data == "Deleted Successfully!!":
             return "Deleted Successfully!!"
-         else:
+        else:
             return 'OOPS!! Failed'
-      else:
-         return 'Content-Type not supported! Send JSON Content'
+    else:
+        return 'Content-Type not supported! Send JSON Content'
 
-'''
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5001, debug=True)
-'''
+    app.run(host='0.0.0.0', port=5001, debug=True)
