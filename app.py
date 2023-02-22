049e6fb210b36cf56e10a8e0056b8916fa897b99
Kavin M <89208380+kavinm21@users.noreply.github.com>
2023-02-22 10:00:29 +0530

    updated html pages served

--- app.py
+++ app.py
@@ -6,34 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('login.html')
-
-
-@app.route('/index', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return redirect(url_for('home'))
-
-    if request.method == 'POST':
-        name = request.form['username']
-        age = request.form['password']
-    return render_template('index.html')
-
-
-@app.route('/logout')
-def logout():
-    return redirect(url_for('home'))
-
-
-@app.route('/display')
-def display():
-    return render_template('display.html')
-
-
-@app.route('/update')
-def update():
-    return render_template('update.html')
-
+    return render_template('login1.html')
 
 @app.route('/api/fetchall', methods=['GET', 'POST'])
 def fetch_all():
